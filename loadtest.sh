b7c5dc4d4d720ebad2dd5deedc560a01c0a9f09e
Himanshu Dutta <meet.himanshu.dutta@gmail.com>
2023-10-11 12:43:19 +0530

    updated config

--- loadtest.sh
+++ loadtest.sh
@@ -8,7 +8,7 @@ sleepTimeSeconds=$3
 #running number of clients
 for i in $(seq 1 $numClients)
 do
-    ./build/loadtestclient 192.168.230.154:5005 source.c $loopNum $sleepTimeSeconds > "./build/client${i}.txt" &
+    ./build/loadtestclient 0.0.0.0:5005 source.c $loopNum $sleepTimeSeconds > "./build/client${i}.txt" &
     pids[${i}]=$!
 done
 
@@ -21,7 +21,6 @@ done
 #giving path to the file going to be created
 tpfile="./build/throughput.csv"
 rtfile="./build/responsetime.csv"
-ndfile="./build/numDropped.csv"
 
 #adding header initially file will not be there
 if [[ ! -f $tpfile ]] 
@@ -34,15 +33,9 @@ then
     echo "M,Avg_Response_time(secs)" > ./build/responsetime.csv
 fi
 
-if [[ ! -f $ndfile ]] 
-then
-    echo "M,Num_Clients_Dropped" > ./build/numDropped.csv
-fi
-
 #read individual throughput, add them and calculate partial response time
 Overall_throughput=0
 pResponsetime=0
-numDropped=0
 for file in ./build/client*.txt
 do
     echo "Looking at $file"
@@ -50,16 +43,10 @@ do
     NSR=$(grep "ART" $file | cut -d ',' -f 2 | cut -d ':' -f 2)
     LT=$(grep "ART" $file | cut -d ',' -f 3 | cut -d ':' -f 2)
 
-    if [[ $ART != "" ]]
-    then
-        throughput_i=$(bc <<< "scale=3; ($NSR * 1000000) / $LT")
-        Overall_throughput=$(bc <<< "scale=3; ($Overall_throughput + $throughput_i)")
-        temp=$(bc <<< "scale=3; ($ART * $loopNum)")
-        pResponsetime=$(bc <<< "scale=3; ($pResponsetime + $temp)")
-    else
-        numDropped=$(($numDropped+1))
-    fi
-
+    throughput_i=$(bc <<< "scale=3; ($NSR * 1000000) / $LT")
+    Overall_throughput=$(bc <<< "scale=3; ($Overall_throughput + $throughput_i)")
+    temp=$(bc <<< "scale=3; ($ART * $loopNum)")
+    pResponsetime=$(bc <<< "scale=3; ($pResponsetime + $temp)")
 done
 #calculate final response time
 total_req=$(($numClients * $loopNum))
@@ -68,10 +55,3 @@ Avg_Response_time=$(bc <<< "scale=3; $pResponsetime / ($total_req*1000000)")
 #sending rows to respective files
 echo "$numClients,$Overall_throughput" >> ./build/throughput.csv
 echo "$numClients,$Avg_Response_time" >> ./build/responsetime.csv
-echo "$numClients,$numDropped" >> ./build/numDropped.csv
-
-if [[ $numDropped -gt 0 ]]
-then
-    exit 1
-fi
-exit 0
