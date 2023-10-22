cb4429869bceb06d1d5d38bd9c596cdc33dd185d
Satyam Shukla <23m0817@iitb.ac.in>
2023-10-22 16:58:43 +0530

    updated servermeasure

--- servermeasure.sh
+++ servermeasure.sh
@@ -3,13 +3,17 @@
 fl="server_stats.csv"
 
 :>$fl
-echo "num_threads,cpu_usage" >> $fl
+echo "num_clients,num_threads,cpu_usage" >> $fl
+
+serverProcId=$1
 
 while true
 do
-
-    numThreads=$(ps huH p 1 | wc -l)
-    cpuUsage=$(vmstat -w | awk 'FNR>2 {print $(NF-3)}')
-    echo "$numThreads,$cpuUsage" >> $fl
+    numClients=$(cat ./numClients.txt | cut -d' ' -f1)
+    numThreads=$(ps -M $serverProcId | grep -v USER | wc -l)
+    # numThreads=$(ps huH p 1 | wc -l)
+    cpuUsage=$(top -l  2 | grep -E "^CPU" | tail -1 | awk '{ print $3 + $5 }')
+    # cpuUsage=$(cpuUsage=$(vmstat -w | awk 'FNR>2 {print $(NF-3)}'))
+    echo "$numClients,$numThreads,$cpuUsage" >> $fl
     sleep 10
 done
