75ede05795947a29900fe0669d9c030e6cd6b54e
Himanshu Dutta <meet.himanshu.dutta@gmail.com>
2023-10-27 20:23:38 +0530

    updated tcp socket functionality

--- include/tcpsocket.h
+++ include/tcpsocket.h
@@ -4,6 +4,8 @@
 #include "types.h"
 
 namespace autograder {
+int setupTCPSocket(std::string ip, short port, int backlog,
+                   bool setReuseAddr = true);
 std::pair<bytes, bool> readFromSocket(int, int);
 bool writeToSocket(int, bytes);
 }  // namespace autograder
--- src/tcpsocket.cc
+++ src/tcpsocket.cc
@@ -6,6 +6,28 @@
 #include "utils.h"
 
 namespace autograder {
+int setupTCPSocket(std::string ip, short port, int backlog, bool setReuseAddr) {
+  int listenerFd;
+  sockaddr_in serverAddr = getSockaddrIn(ip, port);
+  check_error((listenerFd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) >= 0,
+              "tcpsocket::setupTCPSocket - error at socket creation");
+
+  if (setReuseAddr) {
+    const int32_t enable = 1;
+    check_error((setsockopt(listenerFd, SOL_SOCKET, SO_REUSEADDR, &enable,
+                            sizeof(int))) >= 0,
+                "tcpsocket::setupTCPSocket - setsockopt reuseaddr error");
+  }
+
+  check_error(
+      (bind(listenerFd, (sockaddr*)&serverAddr, sizeof(serverAddr))) >= 0,
+      "tcpsocket::setupTCPSocket - bind error");
+  check_error((listen(listenerFd, backlog)) >= 0,
+              "tcpsocket::setupTCPSocket - listen error");
+
+  return listenerFd;
+}
+
 std::pair<bytes, bool> readFromSocket(int fd, int num_bytes) {
   bytes data;
   byte buffer[BUFF_SIZE];
