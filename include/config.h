1ee99b45063b499cd72f96eb0b5d2029d613252b
Himanshu Dutta <meet.himanshu.dutta@gmail.com>
2023-11-30 15:08:52 +0530

    added dependency structure

--- include/config.h
+++ include/config.h
@@ -4,5 +4,5 @@
 
 const int BUFF_SIZE = 8096;
 const int FILENAME_LEN = 8;
-const std::string GRADER_ROOT_DIR = "check";
+const std::string GRADER_ROOT_DIR = "./gradingcache";
 const std::string IDLE_OUT_FILE_PATH = "./samples/idleOutput.txt";
